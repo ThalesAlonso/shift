1b8f89f5e3b1f4d34ffc610b270a2553e795604e
Thales Alonso <koben_rock@hotmail.com>
2021-07-26 11:19:54 -0300

    add feature index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(3000, () => {
+    console.log('Server Running')
+})
+
