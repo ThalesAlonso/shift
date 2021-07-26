517106d38a81e31f231db4fd9b7db63b7fd47d20
Thales Alonso <koben_rock@hotmail.com>
2021-07-26 11:36:32 -0300

    method HTTP

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const routes = require('./config/routes')
 
 
 
@@ -12,6 +13,10 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
+app.use(routes)
+
+
+
 
 
 
