50e2886594e378ab0d70d6e231c808729f604477
Thales Alonso <koben_rock@hotmail.com>
2021-07-26 12:03:53 -0300

    modify routes

--- config/routes.js
+++ config/routes.js
@@ -2,12 +2,48 @@ const express = require('express')
 const routes = express.Router()
 
 
+const examesExameCodigosHemogramaGliclose = ['100', '101']
+const examesExameCodigoDescricaoHemogramaGliclose = ['hemograma', 'glicose']
+const examesExameCodigosT4TSH = ['102', '103']
+const examesExameCodigoDescricaoT4TSH = ['t4', 'tsh']
 
 let db = [
-    {'1': {nome: 'cliente 1', idade: '20'}},
-    {'2': {nome: 'cliente 2', idade: '20'}},
-    {'3': {nome: 'cliente 3', idade: '20'}}
-]
+    
+    {'1': {identificador: 'Thales Fernando Ferreira Alonso', 
+           pedidoCodigo: 'Codigo 1',
+           pedidoOrdemServico: 10 ,
+           pacienteCodigo: 20,
+           pacienteNome: 'Joao Silva',
+           pacienteSexo: 'Masculino',
+           examesExameDescricao:  examesExameCodigosHemogramaGliclose,
+           examesExameCodigo : examesExameCodigoDescricaoHemogramaGliclose
+    }},
+
+
+
+    {'2': {identificador: 'Yasmin Cruza Da Silva Alonso', 
+           pedidoCodigo: 'Codigo 2',
+           pedidoOrdemServico: 20 ,
+           pacienteCodigo: 30,
+           pacienteNome: 'Maria Silva',
+           pacienteSexo: 'Feminino',
+           examesExameDescricao:  examesExameCodigoDescricaoT4TSH,
+           examesExameCodigo : examesExameCodigosT4TSH
+    }},
+    
+
+
+    {'3': {identificador: 'Mario Andrade de Souza', 
+           pedidoCodigo: 'Codigo 3',
+           pedidoOrdemServico: 30 ,
+           pacienteCodigo: 40,
+           pacienteNome: 'Paulo Robson',
+           pacienteSexo: 'Masculino',
+           examesExameDescricao:  examesExameCodigosHemogramaGliclose,
+           examesExameCodigo : examesExameCodigoDescricaoHemogramaGliclose
+    }}
+    ]
+
 
 
 routes.get('/', ( req, res) => {
